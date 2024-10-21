13a964c6d631df4d43566d1a98e91a0a2ce1aee4
LissethVasquez04 <vasquezlisseth004@gmail.com>
2024-10-20 22:09:17 -0600

    Agregar archivos de proyecto.

---
+++ LLVG.Temperatura.UnitTestMSTest/TemperaturaTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LLVG.Temperatura.UnitTestMSTest
+{
+    [TestClass]
+    public class TemperaturaTest
+    {
+        [TestMethod()]
+        public void CelsiusToFahrenheitTest()
+        {
+            Temperatura calculator = new Temperatura();
+
+            double result = calculator.CelsiusToFahrenheit(0);
+
+            Assert.AreEqual(32, result);
+        }
+
+        [TestMethod()]
+        public void FahrenheitToCelsiusTest()
+        {
+            Temperatura calculator = new Temperatura();
+
+            double result = calculator.FahrenheitToCelsius(32);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod()]
+        public void CelsiusToFahrenheitTest_Positive()
+        {
+            Temperatura calculator = new Temperatura();
+            double result = calculator.CelsiusToFahrenheit(100);
+
+            Assert.AreEqual(212, result);
+        }
+
+        [TestMethod()]
+        public void FahrenheitToCelsiusTest_Positive()
+        {
+            Temperatura calculator = new Temperatura();
+
+            double result = calculator.FahrenheitToCelsius(212);
+
+            Assert.AreEqual(100, result);
+        }
+
+        [TestMethod()]
+        public void CelsiusToFahrenheitTest_Negative()
+        {
+            Temperatura calculator = new Temperatura();
+
+            double result = calculator.CelsiusToFahrenheit(-40);
+
+            Assert.AreEqual(-40, result);
+        }
+
+        [TestMethod()]
+        public void FahrenheitToCelsiusTest_Negative()
+        {
+            Temperatura calculator = new Temperatura();
+
+            double result = calculator.FahrenheitToCelsius(-40);
+
+            Assert.AreEqual(-40, result);
+        }
+    }
+}
---
+++ LLVG.Temperatura/Temperatura.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LLVG.Temperatura
+{
+    public class Temperatura
+    {
+        // Método para convertir de Celsius a Fahrenheit
+        public double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+
+        // Método para convertir de Fahrenheit a Celsius
+        public double FahrenheitToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5 / 9;
+        }
+    }
+}
